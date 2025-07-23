4d13d5c47a93fa3fdfcf7223e85bc69fd3340cbe
LtxPgm <ltxprogrammer@outlook.com>
2025-07-23 15:20:56 -0500

    Brought back embedded mod resources for 1.20.1
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/data/JarredPackResources.java
+++ src/main/java/net/ltxprogrammer/changed/data/JarredPackResources.java
@@ -3,23 +3,28 @@ package net.ltxprogrammer.changed.data;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+import com.mojang.logging.LogUtils;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.AbstractPackResources;
 import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.IoSupplier;
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
 
 import javax.annotation.Nullable;
 import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-/*public class BuiltinPackResources extends AbstractPackResources {
+public class JarredPackResources extends AbstractPackResources {
+    private static final Logger LOGGER = LogUtils.getLogger();
     private final File file;
     private final String prefix;
     private final Splitter splitter;
@@ -28,8 +33,8 @@ import java.util.zip.ZipFile;
     @Nullable
     private ZipFile zipFile;
 
-    public BuiltinPackResources(File file, String prefix) {
-        super(new File(file, prefix));
+    public JarredPackResources(String packName, File file, String prefix) {
+        super(packName, true);
         this.file = file;
         this.prefix = prefix;
         this.prefixDirCount = Path.of(prefix).getNameCount();
@@ -44,21 +49,52 @@ import java.util.zip.ZipFile;
         return this.zipFile;
     }
 
-    protected InputStream getResource(String name) throws IOException {
-        ZipFile zipfile = this.getOrCreateZipFile();
-        ZipEntry zipentry = zipfile.getEntry(prefix + name);
-        if (zipentry == null) {
-            throw new ResourcePackFileNotFoundException(this.file, name);
-        } else {
-            return zipfile.getInputStream(zipentry);
+    public @Nullable IoSupplier<InputStream> getResource(String resolvedPath) {
+        try {
+            ZipFile zipfile = this.getOrCreateZipFile();
+            ZipEntry zipentry = zipfile.getEntry(prefix + resolvedPath);
+            return zipentry != null ? IoSupplier.create(zipfile, zipentry) : null;
+        } catch (IOException e) {
+            return null;
         }
     }
 
-    public boolean hasResource(String name) {
+    @Override
+    public @Nullable IoSupplier<InputStream> getRootResource(String... paths) {
+        return this.getResource(String.join("/", paths));
+    }
+
+    @Override
+    public @Nullable IoSupplier<InputStream> getResource(PackType packType, ResourceLocation resourceLocation) {
+        return this.getResource(packType.getDirectory() + "/" + resourceLocation.getNamespace() + "/" + resourceLocation.getPath());
+    }
+
+    @Override
+    public void listResources(PackType packType, String namespace, String path, ResourceOutput output) {
+        ZipFile zipfile;
         try {
-            return this.getOrCreateZipFile().getEntry(prefix + name) != null;
+            zipfile = this.getOrCreateZipFile();
         } catch (IOException ioexception) {
-            return false;
+            return;
+        }
+
+        Enumeration<? extends ZipEntry> enumeration = zipfile.entries();
+        String s = prefix + packType.getDirectory() + "/" + namespace + "/";
+        String s1 = s + path + "/";
+
+        while (enumeration.hasMoreElements()) {
+            ZipEntry zipentry = enumeration.nextElement();
+            if (!zipentry.isDirectory()) {
+                String s2 = zipentry.getName();
+                if (!s2.startsWith(prefix))
+                    continue;
+
+                if (!s2.endsWith(".mcmeta") && s2.startsWith(s1)) {
+                    String s3 = s2.substring(s.length());
+                    output.accept(ResourceLocation.fromNamespaceAndPath(namespace, s3),
+                            IoSupplier.create(zipfile, zipentry));
+                }
+            }
         }
     }
 
@@ -73,7 +109,7 @@ import java.util.zip.ZipFile;
         Enumeration<? extends ZipEntry> enumeration = zipfile.entries();
         Set<String> set = Sets.newHashSet();
 
-        while(enumeration.hasMoreElements()) {
+        while (enumeration.hasMoreElements()) {
             ZipEntry zipentry = enumeration.nextElement();
             String s = zipentry.getName();
             if (s.startsWith(prefix + type.getDirectory() + "/")) {
@@ -83,7 +119,7 @@ import java.util.zip.ZipFile;
                     if (s1.equals(s1.toLowerCase(Locale.ROOT))) {
                         set.add(s1);
                     } else {
-                        this.logWarning(s1);
+                        LOGGER.warn("Ignored non-lowercase namespace: {} in {}", s1, this.file);
                     }
                 }
             }
@@ -104,37 +140,4 @@ import java.util.zip.ZipFile;
         }
 
     }
-
-    public Collection<ResourceLocation> getResources(PackType type, String namespace, String path, int maxDepth, Predicate<String> filter) {
-        ZipFile zipfile;
-        try {
-            zipfile = this.getOrCreateZipFile();
-        } catch (IOException ioexception) {
-            return Collections.emptySet();
-        }
-
-        Enumeration<? extends ZipEntry> enumeration = zipfile.entries();
-        List<ResourceLocation> list = Lists.newArrayList();
-        String s = prefix + type.getDirectory() + "/" + namespace + "/";
-        String s1 = s + path + "/";
-
-        while(enumeration.hasMoreElements()) {
-            ZipEntry zipentry = enumeration.nextElement();
-            if (!zipentry.isDirectory()) {
-                String s2 = zipentry.getName();
-                if (!s2.startsWith(prefix))
-                    continue;
-
-                if (!s2.endsWith(".mcmeta") && s2.startsWith(s1)) {
-                    String s3 = s2.substring(s.length());
-                    String[] astring = s3.split("/");
-                    if (astring.length >= maxDepth + 1 && filter.test(astring[astring.length - 1])) {
-                        list.add(ResourceLocation.fromNamespaceAndPath(namespace, s3));
-                    }
-                }
-            }
-        }
-
-        return list;
-    }
-}*/
+}
